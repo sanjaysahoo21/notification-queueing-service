71f041038d7e15a60127a9ee0067cbbb3df27695
Sanjay Eathakottu <sanjaysahoo2101@gmail.com>
2026-01-19 15:03:10 +0530

    modified global exception handler and app.properties

--- api/src/main/java/com/example/notification/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/example/notification/api/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.example.notification.api.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,18 +13,14 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleExceptionErrors(MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException ex) {
 
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult()
-                .getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
 
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(errors);
-
+        return errors;
     }
-
 }
